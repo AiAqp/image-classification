bb3d276bff7f10458cd36aabaa9f80fca1d0117a
AiAqp <paweszetela@gmail.com>
2023-12-21 18:56:01 +0100

    added datasets

--- dataset.py
+++ dataset.py
@@ -2,7 +2,29 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 
-def get_transforms():
+_dataset_loaders = {
+    'CIFAR-10' : datasets.CIFAR10,
+    'CIFAR-100' : datasets.CIFAR100
+}
+
+def load_dataset(dateset_config):
+    """Load datasets based on the provided configuration."""
+    loader = _dataset_loaders[dateset_config['dataset']]
+    train_transform, test_transform = _get_transforms()
+
+    train_set = loader(dateset_config['root'], train=True, download=True, transform=train_transform)
+    train_loader = DataLoader(train_set, dateset_config['batch_size'], shuffle=True)
+
+    test_set = loader(dateset_config['root'], train=False, download=True, transform=test_transform)
+    test_loader = DataLoader(test_set, dateset_config['batch_size'], shuffle=False)
+
+    return train_loader, test_loader
+
+def _get_transforms(custom_transforms = None):
+    """Get default or custom transforms for training and testing."""
+    if custom_transforms:
+        return custom_transforms
+    
     train_transform = transforms.Compose([
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
@@ -15,21 +37,3 @@ def get_transforms():
     ])
 
     return train_transform, test_transform
-
-def load_cifar10(root, batch_size, train_transform, test_transform):
-    train_set = datasets.CIFAR10(root=root, train=True, download=True, transform=train_transform)
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-
-    test_set = datasets.CIFAR10(root=root, train=False, download=True, transform=test_transform)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
-
-    return train_loader, test_loader
-
-def get_data_loaders(config):
-    train_transform, test_transform = get_transforms()
-    root = config['data_dir']
-    batch_size = config['batch_size']
-
-    train_loader, test_loader = load_cifar10(root, batch_size, train_transform, test_transform)
-    
-    return train_loader, test_loader
