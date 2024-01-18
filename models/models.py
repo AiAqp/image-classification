212cea36b7e6af1562e0ad9a6dbce3475292f345
AiAqp <paweszetela@gmail.com>
2024-01-18 23:47:23 +0100

    size agnostic models

--- models/models.py
+++ models/models.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn 
 import torch.nn.functional as F
 
+from typing import Tuple, List, Union
+
 from .utils import register_model
 
 __all__ = ['BasicCNN', 'FullyConnected']
@@ -9,22 +11,26 @@ __all__ = ['BasicCNN', 'FullyConnected']
 @register_model()
 class BasicCNN(nn.Module):
     """
-    A simple Convolutional Neural Network (CNN) model.
+    A simple Convolutional Neural Network (CNN).
+
+    Args:
+        n_channels (int): The number of channels of input image.
+        n_classes (int): The number of output classes.
     """
-    def __init__(self, n_classes: int) -> None:
+    def __init__(self, n_channels: int, n_classes: int) -> None:
         super(BasicCNN, self).__init__()
-        # Convolutional layers
-        self.conv1 = nn.Conv2d(3, 6, 5)  
+        self.conv1 = nn.Conv2d(n_channels, 6, 5)  
         self.conv2 = nn.Conv2d(6, 16, 5) 
-        # Fully connected layers
-        self.fc1 = nn.Linear(16 * 5 * 5, 120) 
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((1, 1))  
+        self.fc1 = nn.Linear(16, 120)  
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, n_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2)) 
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2) 
-        x = torch.flatten(x, 1)
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = self.adaptive_pool(x) 
+        x = torch.flatten(x, 1)    
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
@@ -33,16 +39,21 @@ class BasicCNN(nn.Module):
 @register_model()
 class FullyConnected(nn.Module):
     """
-    A simple Fully Connected (FC) model.
+    A simple Fully Connected (FC) neural network.
+
+    Args:
+        input_shape (Union[Tuple[int, int, int], List[int]]): The shape of the input tensor as (n_channels, height, width).
+        n_classes (int): The number of output classes.
     """
-    def __init__(self, n_classes: int):
+    def __init__(self, input_shape: Union[Tuple[int, int, int], List[int]], n_classes: int):
         super(FullyConnected, self).__init__()
-        self.fc1 = nn.Linear(3 * 32 * 32, 512)
+        n_channels, height, width = input_shape
+        self.fc1 = nn.Linear(n_channels * height * width, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, n_classes)
 
     def forward(self, x):
-        x = x.view(-1, 3 * 32 * 32)
+        x = x.view(-1, self.fc1.in_features)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
