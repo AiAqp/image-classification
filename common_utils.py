9520faaf9f52e635786aa204bdd2f200bbe15c85
AiAqp <paweszetela@gmail.com>
2024-01-18 21:43:31 +0100

    refactor get_object to import_object

--- common_utils.py
+++ common_utils.py
@@ -1,5 +1,6 @@
 import torch
 import random
+import importlib
 import numpy as np
 from torch.nn import Module
 from typing import Any, List, Dict, Optional, TypeVar
@@ -20,17 +21,22 @@ def save_model(model: Module, path: str) -> None:
     torch.save(model.state_dict(), path)
 
 
-ModuleType = TypeVar('ModuleType')
+def import_object(full_path: str, allowed_names: Optional[List[str]] = None) -> Any:
+    """
+    Dynamically retrieves an object from a given full path (module plus object name).
+    """
+    module_path, object_name = full_path.rsplit('.', 1)
 
-def get_object(package: ModuleType, object_name: str, allowed_names: Optional[List[str]] = None, **kwargs) -> Any:
-    """
-    Dynamically retrieves an object from a given package and initializes it with provided keyword arguments.
-    """
     if allowed_names is not None and object_name not in allowed_names:
         raise ValueError(f"Object name '{object_name}' is not allowed. Allowed objects are: {allowed_names}")
 
     try:
-        obj = getattr(package, object_name)
-        return obj(**kwargs)
+        module = importlib.import_module(module_path)
+
+        return getattr(module, object_name)
+    
     except AttributeError:
-        raise AttributeError(f"Object named '{object_name}' not found in the provided package.")
+        raise AttributeError(f"Object named '{object_name}' not found in the module '{module_path}'.")
+    
+    except ImportError:
+        raise ImportError(f"Module named '{module_path}' could not be imported.")
